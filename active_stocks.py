2a00bd81b0b9d8d8c5d1a78643ff17fe0ed1649e
Sambath S <10422662+sambaths@users.noreply.github.com>
2025-04-06 23:21:39 +0530

    Update active_stocks.py

--- active_stocks.py
+++ active_stocks.py
@@ -6,8 +6,10 @@ from collections import defaultdict
 HEADERS = {
     "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                    "AppleWebKit/537.36 (KHTML, like Gecko) "
-                   "Chrome/115.0.0.0 Safari/537.36"),
-    "Accept-Language": "en-US,en;q=0.9",
+                   "Chrome/135.0.0.0 Safari/537.36"),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-AU,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
 }
 
 # Function to fetch data from a given NSE API endpoint
